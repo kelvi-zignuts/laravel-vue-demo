f869f17daf689b611c9fdc20791ec5858e2c1bb5
kelvi-zignuys <kelvib@zignuts.com>
2024-05-29 12:07:54 +0530

    authentication using laravel and vue.js

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="w-6/12 p-10 mx-auto">
+        <div class="flex justify-between">
+            <h1 class="text-2xl"> Dashboard </h1>
+            <span class="capitalize">Welcome {{ user && user.name }}, <button
+                class="text-orange-500 underline hover:no-underline rounded-md"
+                @click="handleLogout">Logout</button></span>
+        </div>
+    </div>
+</template>
+<script>
+import { ref, onMounted } from 'vue'
+import { useRouter } from "vue-router";
+import { request } from '../helper'
+import Loader from '../components/Loader.vue';
+
+export default {
+    components: {
+        Loader,
+    },
+    setup() {
+        const user = ref(null);
+        const isLoading = ref(false);
+
+        let router = useRouter();
+        onMounted(() => {
+            authentication();
+        });
+
+        const authentication = async () => {
+            isLoading.value = true;
+            try {
+                const req = await request('get', '/api/user');
+                user.value = req.data;
+                if (user.value.role === 'admin') {
+                    await router.push('/admin-dashboard'); // Redirect to admin dashboard
+                } else {
+                    await router.push('/user-dashboard'); // Redirect to user dashboard
+                }
+            } catch (e) {
+                await router.push('/');
+            } finally {
+                isLoading.value = false;
+            }
+        }
+
+        const handleLogout = () => {
+            localStorage.removeItem('APP_DEMO_USER_TOKEN');
+            router.push('/');
+        }
+
+        return {
+            user,
+            isLoading,
+            handleLogout,
+        }
+    },
+}
+</script>
